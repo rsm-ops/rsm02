1d0537531ec0303d865976c0f012251e8c883d88
rsm <1821469810@qq.com>
2021-04-01 15:48:42 +0800

    第一次更改

--- HelloWorld/src/main/java/Hello.java
+++ HelloWorld/src/main/java/Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("你好世界");
+	System.out.println("哈哈世界");
+System.out.println("你好");
+System.out.println("你");
     }
 }
